cc33125e0a942b6064575d18b2c4f6d84df02040
cquiogue <83323770+cquiogue@users.noreply.github.com>
2023-04-13 19:01:16 -0400

    Created basic layout and content

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling to anchor links
+$('a[href^="#"]').click(function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+  });
