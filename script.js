427d50d6ffb807b4acaf94e1713405bce9541576
cquiogue <83323770+cquiogue@users.noreply.github.com>
2023-04-25 18:29:12 -0400

    updated social links

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 // Smooth scrolling to anchor links
-$('a[href^="#"]').click(function(event) {
-    var target = $(this.getAttribute('href'));
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-  });
+$('a[href^="#"]').click(function (event) {
+  var target = $(this.getAttribute('href'));
+  if (target.length) {
+    event.preventDefault();
+    $('html, body').stop().animate(
+      {
+        scrollTop: target.offset().top,
+      },
+      1000
+    );
+  }
+});
+
+// Get form element
+const form = document.querySelector('form');
+
+// Add submit event listener
+form.addEventListener('submit', function (event) {
+  event.preventDefault(); // prevent form submission
+
+  // Get form data
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  // Create mailto URL
+  const mailtoUrl = `mailto:cquiogue@gmail.com?subject=Contact Form Submission&body=Name: ${name}%0D%0AEmail: ${email}%0D%0AMessage: ${message}`;
+
+  // Open mailto URL in new window
+  window.open(mailtoUrl, '_blank');
+});
